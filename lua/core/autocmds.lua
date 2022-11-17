03c6835feb86f2243e82ee438558c1076a78135d
Ahanaf Ether <etherrorcode404@gmail.com>
2022-11-17 14:34:15 +0600

    updates

---
+++ lua/core/autocmds.lua
@@ -0,0 +1,99 @@
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+-----------------------------------------------------------
+-- General Settings
+-----------------------------------------------------------
+-- Briefly highlight a selection on yank
+augroup('YankHighlight', { clear = true })
+autocmd('TextYankPost', {
+  group = 'YankHighlight',
+  callback = function()
+    vim.highlight.on_yank()
+  end
+})
+
+-- Set relative number if in normal mode
+augroup('RelativeNumber', { clear = true })
+autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+  pattern = "*",
+  group = 'RelativeNumber',
+  callback = function()
+    if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
+       vim.opt.relativenumber = true
+    end
+end,
+})
+
+-- Set absolute number if in normal mode
+augroup('AbsoluteNumber', { clear = true })
+autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
+  pattern = "*",
+  group = 'AbsoluteNumber',
+  callback = function()
+    if vim.o.nu then
+       vim.opt.relativenumber = false
+       vim.cmd "redraw"
+    end
+end,
+})
+
+-- Remove whitespace on save
+autocmd('BufWritePre', {
+  pattern = '',
+  command = ":%s/\\s\\+$//e"
+})
+
+-- Don't auto commenting new lines
+autocmd('BufEnter', {
+  pattern = '',
+  command = 'set fo-=c fo-=r fo-=o'
+})
+
+-----------------------------------------------------------
+-- Filetype Settings
+-----------------------------------------------------------
+-- Disable line length marker
+augroup('setLineLength', { clear = true })
+autocmd('Filetype', {
+  group = 'setLineLength',
+  pattern = { 'text', 'markdown', 'html', 'xhtml', 'javascript', 'typescript' },
+  command = 'setlocal cc=0'
+})
+
+-- Set indentation to 2 spaces
+augroup('setIndent', { clear = true })
+autocmd('Filetype', {
+  group = 'setIndent',
+  pattern = { 'xml', 'html', 'xhtml', 'css', 'scss', 'javascript', 'typescript',
+    'yaml', 'lua'
+  },
+  command = 'setlocal shiftwidth=2 tabstop=2'
+})
+
+-----------------------------------------------------------
+-- Terminal Settings
+-----------------------------------------------------------
+-- Open a Terminal on the right tab
+autocmd('CmdlineEnter', {
+  command = 'command! Term :botright vsplit term://$SHELL'
+})
+
+-- Enter insert mode when switching to terminal
+autocmd('TermOpen', {
+  command = 'setlocal listchars= nonumber norelativenumber nocursorline',
+})
+
+autocmd('TermOpen', {
+  pattern = '',
+  command = 'startinsert'
+})
+
+-- Close terminal buffer on process exit
+autocmd('BufLeave', {
+  pattern = 'term://*',
+  command = 'stopinsert'
+})
+
+-- Define autocommands with Lua APIs
+-- See: h:api-autocmd, h:augroup
