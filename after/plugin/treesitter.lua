52ec1fb716aaa2688ee6dfe6dc847866fa08c1ba
Ether <etherrorcode404@gmail.com>
2023-01-12 00:57:43 +0600

    Update treesitter.lua

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,11 +1,17 @@
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "rust", "ruby", "vim", "python", "html", "css"  },
+  ensure_installed = { "c", "lua", "rust", "ruby", "vim", "python", "html", "css" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+  sync_install = true,
   auto_install = true,
   highlight = {
+    enable = true,
+  },
+  indent = {
+    enable = true,
+  },
+  autotag = {
     enable = true,
   },
 }
