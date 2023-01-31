234988476df22dd2a679d5b2bf71928fac7a6fcb
Ether <etherrorcode404@gmail.com>
2023-01-31 22:12:22 +0600

    Revert "images"
    This reverts commit 1b68db32fee7d032f85491bc7f6227f1c80e693b.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -21,12 +21,6 @@ telescope.setup {
     },
   },
   extensions = {
-    media_files = {
-      -- filetypes whitelist
-      -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-      filetypes = { "png", "webp", "jpg", "jpeg" },
-      find_cmd = "rg" -- find command (defaults to `fd`)
-    },
     file_browser = {
       theme = "ivy",
       -- theme = "dropdown",
@@ -55,7 +49,6 @@ telescope.setup {
 }
 
 telescope.load_extension("file_browser")
-telescope.load_extension("media_files")
 
 vim.keymap.set('n', ';f',
   function()
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -255,11 +255,6 @@ _G.packer_plugins = {
     path = "/home/ether/.local/share/nvim/site/pack/packer/start/telescope-file-browser.nvim",
     url = "https://github.com/nvim-telescope/telescope-file-browser.nvim"
   },
-  ["telescope-media-files.nvim"] = {
-    loaded = true,
-    path = "/home/ether/.local/share/nvim/site/pack/packer/start/telescope-media-files.nvim",
-    url = "https://github.com/nvim-telescope/telescope-media-files.nvim"
-  },
   ["telescope.nvim"] = {
     loaded = true,
     path = "/home/ether/.local/share/nvim/site/pack/packer/start/telescope.nvim",
