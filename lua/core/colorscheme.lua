ac4c538ce0bad7d7f3e80c03e7643c4909df921c
Ether <etherrorcode404@gmail.com>
2023-02-12 02:00:55 +0600

    updats

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -117,16 +117,17 @@ vim.api.nvim_create_autocmd('ColorScheme', {
     pattern = 'gruvbox-material',
     command =
     'hi LineNr                                 guibg=#282828  |' ..
-    'hi CursorLine                         guibg=#3c3836  |' ..
+    -- 'hi CursorLine                         guibg=#3c3836  |' ..
     'hi CursorLineNr                       guibg=#282828  |' ..
     --'hi CursorLineNr                       guifg=#e9b143  |' ..
-    'hi MatchParen                         guifg=#f28534  |' ..
+    -- 'hi MatchParen                         guifg=#f28534  |' ..
     --'hi MatchParen                         guibg=#665c54  |' ..
     'hi ColorColumn                        guibg=#3c3836  |' ..
     'hi DiagnosticVirtualTextError         guifg=#db4740 guibg=#3c1f1e |' ..
     'hi DiagnosticVirtualTextWarn          guifg=#e9b143 guibg=#473c29 |' ..
-    'hi DiagnosticVirtualTextInfo          guifg=#80aa9e guibg=#2e3b3b |' ..
     'hi DiagnosticVirtualTextHint          guifg=#8bba7f guibg=#333e34 |' ..
+    -- 'hi DiagnosticVirtualTextHint          guifg=#bf68d9 guibg=#282c34 |' ..
+    'hi DiagnosticVirtualTextInfo          guifg=#80aa9e guibg=#2e3b3b |' ..
     'hi NvimTreeNormal                     guibg=#181818 |' ..
     'hi NvimTreeEndOfBuffer                guibg=#181818 |' ..
     'hi NoiceCmdlinePopupBorderCmdline     guifg=#ea6962 guibg=#1d021 |' ..
@@ -135,7 +136,7 @@ vim.api.nvim_create_autocmd('ColorScheme', {
     'hi TelescopePromptTitle               guifg=#1d2021 guibg=#89b482 |' ..
     'hi TelescopePromptPrefix              guifg=#ea6962 guibg=#504945 |' ..
     'hi TelescopePromptCounter             guifg=#e2cca9 guibg=#504945 |' ..
-    'hi TelescopePreviewTitle              guifg=#ea6962 guibg=#1d2021 |' ..
+    'hi TelescopePreviewTitle              guifg=#e2cca9 guibg=#1d2021 |' ..
     -- 'hi TelescopePreviewTitle              guifg=#e2cca9 guibg=#1d2021 |' ..
     -- 'hi TelescopePreviewTitle              guifg=#1d2021 guibg=#ea6962 |' ..
     'hi TelescopePreviewBorder             guifg=#282828 guibg=#282828 |' ..
@@ -171,40 +172,42 @@ vim.api.nvim_create_autocmd('ColorScheme', {
     pattern = 'gruvbox',
     command =
     'hi LineNr                             guibg=#282828  |' ..
+    'hi CursorLine                         guibg=#282828  |' ..
     'hi CursorLineNr                       guibg=#282828  |' ..
     'hi DiagnosticVirtualTextError         guifg=#db4740 guibg=#3c1f1e |' ..
     'hi DiagnosticVirtualTextWarn          guifg=#e9b143 guibg=#473c29 |' ..
     'hi DiagnosticVirtualTextInfo          guifg=#80aa9e guibg=#2e3b3b |' ..
     'hi DiagnosticVirtualTextHint          guifg=#8bba7f guibg=#333e34 |' ..
-    'hi NvimTreeNormal                     guibg=#181818 |' ..
-    'hi NvimTreeEndOfBuffer                guibg=#181818 |' ..
-    'hi TelescopePromptBorder              guifg=#ea6962 guibg=#282828 |' ..
-    'hi NoiceCmdlinePopupBorderCmdline     guifg=#ea6962 guibg=#282828 |' ..
-    'hi TelescopePromptNormal              guifg=#ea6962 guibg=#282828 |' ..
-    'hi TelescopePromptBorder              guifg=#ea6962 guibg=#282828 |' ..
-    'hi TelescopePromptTitle               guifg=#ea6962 guibg=#282828 |' ..
-    'hi TelescopePromptPrefix              guifg=#ea6962 guibg=#282828 |' ..
-    'hi TelescopePromptCounter             guifg=#ea6962 guibg=#282828 |' ..
-    'hi TelescopePreviewTitle              guifg=#89b482 guibg=#282828 |' ..
-    'hi TelescopePreviewBorder             guifg=#89b482 guibg=#282828 |' ..
-    'hi TelescopeResultsTitle              guifg=#89b482 guibg=#282828 |' ..
-    'hi TelescopeResultsBorder             guifg=#89b482 guibg=#282828 |' ..
-    'hi TelescopeMatching                  guifg=#d8a657 guibg=#282828 |' ..
-    'hi TelescopeSelection                 guifg=#ffffff guibg=#32302f |' ..
-    'hi FloatBorder                        guifg=#ea6962 guibg=#282828 |' ..
-    'hi BqfPreviewBorder                   guifg=#ea6962 guibg=#282828 |' ..
-    'hi NormalFloat                        guibg=#282828 |' ..
-    'hi IndentBlanklineContextChar         guifg=#d3869b |' ..
-    'hi markid1                            guifg=#ff8f88 |' ..
-    'hi markid2                            guifg=#ffb074 |' ..
-    'hi markid3                            guifg=#cfdc8b |' ..
-    'hi markid4                            guifg=#a3d4c9 |' ..
-    'hi markid5                            guifg=#f9acc1 |' ..
-    'hi markid6                            guifg=#afdaa8 |' ..
-    'hi markid7                            guifg=#fecc7d |' ..
-    'hi markid8                            guifg=#eed8b2 |' ..
-    'hi markid9                            guifg=#ffedc7 |' ..
-    'hi markid10                           guifg=#cebfaa |' ..
+    'hi MatchParen                         guifg=#fe8019  |' ..
+    -- 'hi NvimTreeNormal                     guibg=#181818 |' ..
+    -- 'hi NvimTreeEndOfBuffer                guibg=#181818 |' ..
+    -- 'hi TelescopePromptBorder              guifg=#ea6962 guibg=#282828 |' ..
+    -- 'hi NoiceCmdlinePopupBorderCmdline     guifg=#ea6962 guibg=#282828 |' ..
+    -- 'hi TelescopePromptNormal              guifg=#ea6962 guibg=#282828 |' ..
+    -- 'hi TelescopePromptBorder              guifg=#ea6962 guibg=#282828 |' ..
+    -- 'hi TelescopePromptTitle               guifg=#ea6962 guibg=#282828 |' ..
+    -- 'hi TelescopePromptPrefix              guifg=#ea6962 guibg=#282828 |' ..
+    -- 'hi TelescopePromptCounter             guifg=#ea6962 guibg=#282828 |' ..
+    -- 'hi TelescopePreviewTitle              guifg=#89b482 guibg=#282828 |' ..
+    -- 'hi TelescopePreviewBorder             guifg=#89b482 guibg=#282828 |' ..
+    -- 'hi TelescopeResultsTitle              guifg=#89b482 guibg=#282828 |' ..
+    -- 'hi TelescopeResultsBorder             guifg=#89b482 guibg=#282828 |' ..
+    -- 'hi TelescopeMatching                  guifg=#d8a657 guibg=#282828 |' ..
+    -- 'hi TelescopeSelection                 guifg=#ffffff guibg=#32302f |' ..
+    -- 'hi FloatBorder                        guifg=#ea6962 guibg=#282828 |' ..
+    -- 'hi BqfPreviewBorder                   guifg=#ea6962 guibg=#282828 |' ..
+    -- 'hi NormalFloat                        guibg=#282828 |' ..
+    -- 'hi IndentBlanklineContextChar         guifg=#d3869b |' ..
+    -- 'hi markid1                            guifg=#ff8f88 |' ..
+    -- 'hi markid2                            guifg=#ffb074 |' ..
+    -- 'hi markid3                            guifg=#cfdc8b |' ..
+    -- 'hi markid4                            guifg=#a3d4c9 |' ..
+    -- 'hi markid5                            guifg=#f9acc1 |' ..
+    -- 'hi markid6                            guifg=#afdaa8 |' ..
+    -- 'hi markid7                            guifg=#fecc7d |' ..
+    -- 'hi markid8                            guifg=#eed8b2 |' ..
+    -- 'hi markid9                            guifg=#ffedc7 |' ..
+    -- 'hi markid10                           guifg=#cebfaa |' ..
     'hi CodewindowBorder                   guifg=#ea6962 |'
 })
 
