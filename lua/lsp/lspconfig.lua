bcb05b9e3721445286d7eee6920d26d8522d8c80
Ether <etherrorcode404@gmail.com>
2023-02-22 16:47:16 +0600

    Update lspconfig.lua

--- lua/lsp/lspconfig.lua
+++ lua/lsp/lspconfig.lua
@@ -120,6 +120,11 @@ nvim_lsp.cssls.setup {
   capabilities = capabilities
 }
 
+nvim_lsp.clangd.setup {
+  on_attach = on_attach,
+  capabilities = capabilities
+}
+
 nvim_lsp.tailwindcss.setup {
   on_attach = on_attach,
   capabilities = capabilities
