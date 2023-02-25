ecbc7aafb02c2dfd78d9695d5f30903c4bd9a3c8
Ether <etherrorcode404@gmail.com>
2023-02-26 00:31:43 +0600

    Update nvim-autopairs.lua

--- after/plugin/nvim-autopairs.lua
+++ after/plugin/nvim-autopairs.lua
@@ -2,11 +2,11 @@ local autopairs_status_ok, autopairs = pcall(require, 'nvim-autopairs')
 if not autopairs_status_ok then return end
 
 autopairs.setup({
-    disable_filetype = { "TelescopePrompt", "vim" },
-    check_ts = true,
-    ts_config = {
-        lua = { 'string' }, -- it will not add a pair on that treesitter node
-        javascript = { 'template_string' },
-        java = false, -- don't check treesitter on java
-    }
+  disable_filetype = { "TelescopePrompt", "vim" },
+  check_ts = true,
+  ts_config = {
+    lua = { 'string' }, -- it will not add a pair on that treesitter node
+    javascript = { 'template_string' },
+    java = false, -- don't check treesitter on java
+  }
 })
