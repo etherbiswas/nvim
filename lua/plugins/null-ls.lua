77924de26dc9463f50c4da91083dcaecd7e08519
Ether Biswas <etherrorcode404@gmail.com>
2024-03-21 12:29:42 +0600

    minor changes

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -24,9 +24,6 @@ return {
         sources = {
           null_ls.builtins.formatting.prettierd,
           null_ls.builtins.formatting.fish_indent,
-          null_ls.builtins.diagnostics.eslint_d.with({
-            diagnostics_format = '[eslint] #{m}\n(#{c})'
-          }),
           null_ls.builtins.diagnostics.fish,
           null_ls.builtins.diagnostics.tidy
         },
